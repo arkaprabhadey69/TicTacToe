d15f3aa52bf30db934dcdc9523023abc74bfc01b
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-07 10:43:23 +0530

    [Arka]Added:Display board

--- src/com/bl/tictac/TicTacToeProblem.java
+++ src/com/bl/tictac/TicTacToeProblem.java
@@ -5,24 +5,40 @@ import java.util.Scanner;
 public class TicTacToeProblem {
 
     public static Scanner s = new Scanner(System.in);
-//Method to Create Board
+
+    //Method to Create Board
     public static char[] createBoard() {
         char[] board = new char[10];
         for (int position = 1; position < board.length; position++) {
-            board[position] = ' ';
+            board[position] = 'X';
         }
         return board;
     }
-//Method to Choose Letter To Input
-    public static char chooseLetter() {
-        System.out.println("Enter letter: ");
-        char letter = s.next().charAt(0);
+
+    //Method to Choose Letter To Input
+    public static char chooseLetter(char letter) {
+
         return Character.toUpperCase(letter);
     }
 
+    public static void showBoard(char board[]) {
+        System.out.println("\n" + board[1] + "|" + board[2] + "|" + board[3]);
+        System.out.println("*****");
+        System.out.println(board[4] + "|" + board[5] + "|" + board[6]);
+        System.out.println("*****");
+        System.out.println(board[7] + "|" + board[8] + "|" + board[9]);
+
+
+    }
+
     public static void main(String[] args) {
+        System.out.println("Enter letter: ");
+        char letter = s.next().charAt(0);
         char[] board = createBoard();
-        char letter = chooseLetter();
-        System.out.println(letter);
+        char userletter = chooseLetter(letter);
+        char computerletter = (chooseLetter(letter) == 'X' ? 'O' : 'X');
+        showBoard(board);
+
+
     }
 }

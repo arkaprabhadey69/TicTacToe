b03a53be51346b176c43564ace45f9d48f1972ac
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-07 11:58:30 +0530

    [Arka]Added: Validating user move

--- src/com/bl/tictac/TicTacToeProblem.java
+++ src/com/bl/tictac/TicTacToeProblem.java
@@ -1,17 +1,19 @@
 package com.bl.tictac;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TicTacToeProblem {
 
-    public static Scanner s = new Scanner(System.in);
+    public static Scanner userinput = new Scanner(System.in);
 
     //Method to Create Board
     public static char[] createBoard() {
         char[] board = new char[10];
         for (int position = 1; position < board.length; position++) {
-            board[position] = 'X';
+            board[position] = ' ';
         }
+      //  board[2] = 'X';
         return board;
     }
 
@@ -21,6 +23,7 @@ public class TicTacToeProblem {
         return Character.toUpperCase(letter);
     }
 
+    //Method to Display Board
     public static void showBoard(char board[]) {
         System.out.println("\n" + board[1] + "|" + board[2] + "|" + board[3]);
         System.out.println("*****");
@@ -31,13 +34,38 @@ public class TicTacToeProblem {
 
     }
 
+    //Method to get user move
+    public static int getUserMove(char[] board) {
+        ArrayList<Integer> availablecells = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            availablecells.add(i);
+        }
+        while (true) {
+            System.out.println("Enter your preferred index: ");
+            int index = userinput.nextInt();
+            if (availablecells.contains(index) && board[index] == ' ')
+                return index;
+        }
+    }
+
+//    //Method to check whether cell is free or not
+//    public static boolean isCellFree(char[] board, int index) {
+//        if (board[index] == ' ')
+//            return true;
+//        else
+//            return false;
+//    }
+
+    //MAIN
     public static void main(String[] args) {
         System.out.println("Enter letter: ");
-        char letter = s.next().charAt(0);
+        char letter = userinput.next().charAt(0);
         char[] board = createBoard();
         char userletter = chooseLetter(letter);
         char computerletter = (chooseLetter(letter) == 'X' ? 'O' : 'X');
         showBoard(board);
+        int index = getUserMove(board);
+        System.out.println(index);
 
 
     }
